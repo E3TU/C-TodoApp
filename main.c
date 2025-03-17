0ee26e006fb7b4f3792f77ecb8213a91f11efa63
Eetu <99178022+E3TU@users.noreply.github.com>
2025-03-17 20:25:12 +0200

    Basic logic for todo app

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
+#define TODO_FILE "todos.txt"
+#define TODO_LENGTH 50
+
+struct todo {
+    int id;
+    char description[TODO_LENGTH];
+};
+
+void list_todos() {
+
+}
+
+void add_todo() {
+
+}
+
+void delete_todo() {
+
+}
+
 int main(void)
 {
-    printf("Hello, World!\n");
     return 0;
 }
