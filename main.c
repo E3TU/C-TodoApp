441a0c56632ad8ff5cd02fee01d0ade3495d2187
Eetu <99178022+E3TU@users.noreply.github.com>
2025-10-06 23:39:54 +0300

    Planning the function to create config file
    Planning the function used to create config file which contains the file path to todos.txt

--- main.c
+++ main.c
@@ -1,17 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #define TODO_FILE "todos.txt"
 #define TODO_LENGTH 50
 
 int id;
 
+char default_path[256];
+
+
 typedef struct {
   char description[TODO_LENGTH];
 } Todo;
 
 Todo todo;
+
+void create_config_file(char *path) {
+
+  printf("User input: %s\n", path);
+  const char *home_dir = getenv("HOME");
+
+  const char *username = getenv("USER");
+
+  if (home_dir == NULL) {
+    perror("Error: Home environment variable isnt set");
+    return;
+  } else {
+
+    char dir_path[256];
+
+    snprintf(dir_path, sizeof(dir_path), "%s/.config/c-todoapp", home_dir);
+
+    strcpy(default_path, dir_path);
+
+    if (mkdir(dir_path, 0700) == -1) {
+      perror("Error creating dir for config file");
+      return;
+    } else{
+      printf("Dir '%s' created successfully\n", dir_path);
+
+      char file_path[256];
+      snprintf(file_path, sizeof(file_path), "%s/config.conf", dir_path);
+
+      FILE * file = fopen(file_path, "w");
+      if (file == NULL) {
+        perror("Error opening file");
+        return;
+      } else {
+        fprintf(file, "# This is the path for the todos:\n");
+        fprintf(file, "\n");
+        fprintf(file, "%s", path);
+        fclose(file);
+        printf("File %s created successfully", file_path);
+        return;
+      }
+      return;
+    }
+  }
+}
 
 void list_todos() {
   FILE *file = fopen(TODO_FILE, "r");
@@ -100,6 +148,8 @@ int main(int argc, char *argv[]) {
   } else if (strcmp(argv[1], "delete") == 0 && argc == 3) {
     int kurwa = atoi(argv[2]);
     delete_todo(&kurwa);
+  } else if (strcmp(argv[1], "init") == 0 && argc == 3) {
+    create_config_file(argv[2]);
   }
   return 0;
 }
